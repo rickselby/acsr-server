061c1f0b998ccafa6c43888add0e8b0e8c257d02
Rick Selby <rick@selby-family.co.uk>
2016-12-01 14:14:05 +0000

    LeftRightGrids; Assign Races

--- app/Services/Grids/LeftRightGrids/GenerateGrids.php
+++ app/Services/Grids/LeftRightGrids/GenerateGrids.php
@@ -4,18 +4,24 @@ namespace App\Services\Grids\LeftRightGrids;
 
 class GenerateGrids
 {
-    /** @var  AssignSlots */
+    /** @var AssignSlots */
     protected $assignSlots;
 
-    public function __construct(AssignSlots $assignSlots)
+    /** @var AssignRaces */
+    protected $assignRaces;
+
+    public function __construct(AssignSlots $assignSlots, AssignRaces $assignRaces)
     {
         $this->assignSlots = $assignSlots;
+        $this->assignRaces = $assignRaces;
     }
 
     public function generate(array $drivers, $driversPerHeat, $heatsPerDriver)
     {
         // Put drivers into sessions and slots
         $slots = $this->assignSlots->assign(count($drivers), $driversPerHeat, $heatsPerDriver);
+
+        $races = $this->assignRaces->assign($slots);
 
         // ...continue...!
     }
