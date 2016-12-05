fc5639d3bf5c970dc3300fc7b81b70b32998e72b
Rick Selby <rick@selby-family.co.uk>
2016-12-05 15:26:49 +0000

    Add config file alterer and entry list generator

--- app/Contracts/ConfigFileContract.php
+++ app/Contracts/ConfigFileContract.php
@@ -6,6 +6,7 @@ interface ConfigFileContract
 {
     /**
      * Update the server config with the given options
+     * @param string $config
      * @param array $options
      * @return string
      */
@@ -13,8 +14,9 @@ interface ConfigFileContract
 
     /**
      * Get an entry list for the given drivers
+     * @param string $carModel
      * @param array $drivers
      * @return string
      */
-    public function getEntryList(array $drivers);
+    public function getEntryList($carModel, array $drivers);
 }
